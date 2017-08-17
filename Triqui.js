24ae3824bedbfd38a75469b779beb0668fe84b59
carloszorro <csanchezzorro@gmail.com>
2017-08-16 22:02:07 -0500

    tercer commit

--- Triqui.js
+++ Triqui.js
@@ -1,7 +1,23 @@
-html {
-  background-color: black;
-}
+window.onload = function() {
+  var jugador = "X";
+  var jugador2 = "O";
+  var turno = turno + 1;
+  var que_turno ;
+  var cont = 0;
+  var opcion = document.querySelectorAll("li.button");
 
-p {
-  background-color: black;
-}
+opcion.forEach(function(element,idx){
+      element.addEventListener("click", function(event){
+        cont = cont + 1;
+        if (cont%2 === 1){
+          element.innerText = "O";
+        }else if (cont%2 === 0){
+          element.innerText = "X";
+        }
+      console.log("Number", event.target.innerText);
+      });
+  });
+  function myFunction(){
+    location.reload();
+  }
+};
