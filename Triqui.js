248b3d1161919cf759b069079361b1d728d9f766
Carlos Andres Sánchez Zorro <carlosandressanchezzorro@192.168.0.5>
2017-08-17 14:33:38 -0500

    html y css

--- Triqui.js
+++ Triqui.js
@@ -5,8 +5,32 @@ window.onload = function() {
   var que_turno ;
   var cont = 0;
   var opcion = document.querySelectorAll("li.button");
+  var c1 = document.querySelector("#c1");
+  var c2 = document.querySelector("#c2");
+  var c3 = document.querySelector("#c3");
+  var c4 = document.querySelector("#c4");
+  var c5 = document.querySelector("#c5");
+  var c6 = document.querySelector("#c6");
+  var c7 = document.querySelector("#c7");
+  var c8 = document.querySelector("#c8");
+  var c9 = document.querySelector("#c9");
 
-opcion.forEach(function(element,idx){
+  console.log("c5");
+
+  function triqui(){
+    alert ('triqui');
+  }
+
+  function cargarDocumento(){
+    document.getElementById('c9').addEventListener('click',triqui);
+  }
+
+  window.addEventListener('load',cargarDocumento);
+
+  console.log(triqui);
+
+
+  opcion.forEach(function(element,idx){
       element.addEventListener("click", function(event){
         cont = cont + 1;
         if (cont%2 === 1){
