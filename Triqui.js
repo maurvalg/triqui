d6a939786833ad719a0d9999364bb5f03e6ac2b1
carloszorro <csanchezzorro@gmail.com>
2017-08-17 22:03:13 -0500

    Funcionamiento y ganadores

--- Triqui.js
+++ Triqui.js
@@ -5,47 +5,50 @@ window.onload = function() {
   var que_turno ;
   var cont = 0;
   var opcion = document.querySelectorAll("li.button");
-  var c1 = document.querySelector("#c1");
-  var c2 = document.querySelector("#c2");
-  var c3 = document.querySelector("#c3");
-  var c4 = document.querySelector("#c4");
-  var c5 = document.querySelector("#c5");
-  var c6 = document.querySelector("#c6");
-  var c7 = document.querySelector("#c7");
-  var c8 = document.querySelector("#c8");
-  var c9 = document.querySelector("#c9");
+
 
   opcion.forEach(function(element,idx){
     element.addEventListener("click", function(event){
       console.log(element.innerText);
       cont = cont + 1;
       if (cont%2 === 1 ){
-        if (element.innerText !== "O" && element.innerText !== "X") {
+        if (element.innerText === "") {
           element.innerText = "O";
         }else {
+          alert("Casilla Ocupada por jugador dos");
           cont -= 1;
-          alert("Casilla Ocupada");
         }
       }else {
-        if (element.innerText !=="O" && element.innerText!=="X"){
+        if (element.innerText ===""){
           element.innerText = "X";
+          jugadorx();
         }else {
+          alert("Casilla Ocupada por jugador uno");
           cont -= 1;
-          alert("Casilla Ocupada");
         }
       }
     });
   });
 
   function jugadorx(){
+    var c1 = document.querySelector("#c1").innerText;
+    var c2 = document.querySelector("#c2").innerText;
+    var c3 = document.querySelector("#c3").innerText;
+    var c4 = document.querySelector("#c4").innerText;
+    var c5 = document.querySelector("#c5").innerText;
+    var c6 = document.querySelector("#c6").innerText;
+    var c7 = document.querySelector("#c7").innerText;
+    var c8 = document.querySelector("#c8").innerText;
+    var c9 = document.querySelector("#c9").innerText;
+
     //Horizontal
     if (c1 === jugador1 && c2 === jugador1 && c3 === jugador1) {
-      alert("jugador es el ganador");
+      console.log("jugador1 es el ganador");
     }else if (c4 === jugador1 && c5 === jugador1 && c6 === jugador1) {
-      alert("jugador es el ganador");
+      alert("jugador1 es el ganador");
     }else if (c7 === jugador1 && c8 === jugador1 && c9 === jugador1) {
-      alert("jugador es el ganador");
+      alert("jugador1 es el ganador");
     }
+    //
   }
 };
- 
